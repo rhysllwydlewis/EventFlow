617e76bd9635fe2fa18146009c2249c1d76563bf
rhysllwydlewis <rhysllwydlewis@gmail.com>
2026-07-09 23:44:22 +0100

    Fix v2 navbar renderer placeholder handling

--- utils/template-renderer.js
+++ utils/template-renderer.js
@@ -171,7 +171,7 @@ function replacePlaceholders(content) {
   let result = content;
 
   for (const [key, value] of Object.entries(placeholders)) {
-    const pattern = new RegExp(`\\{\\{${key}\\}`, 'g');
+    const pattern = new RegExp(`\\{\\{${key}\\}}`, 'g');
     result = result.replace(pattern, value);
   }
 
