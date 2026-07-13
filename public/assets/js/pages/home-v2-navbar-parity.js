bd1a83a387dcd9e70e6aa9d9bb8c6fc70b08ef14
rhysllwydlewis <rhysllwydlewis@gmail.com>
2026-07-13 12:11:30 +0100

    Bump V2 mobile sign-up component asset version

--- public/assets/js/pages/home-v2-navbar-parity.js
+++ public/assets/js/pages/home-v2-navbar-parity.js
@@ -15,7 +15,7 @@
       const stylesheet = document.createElement('link');
       stylesheet.id = 'ef-home-mobile-signup-css';
       stylesheet.rel = 'stylesheet';
-      stylesheet.href = '/assets/css/home-mobile-signup.css?v=1.1.0';
+      stylesheet.href = '/assets/css/home-mobile-signup.css?v=1.2.0';
       document.head.appendChild(stylesheet);
     }
 
@@ -24,7 +24,7 @@
     }
 
     const script = document.createElement('script');
-    script.src = '/assets/js/components/home-mobile-signup.js?v=1.1.0';
+    script.src = '/assets/js/components/home-mobile-signup.js?v=1.2.0';
     script.async = true;
     script.dataset.efHomeMobileSignup = 'true';
     document.head.appendChild(script);
