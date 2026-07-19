f00c8f68150429a07f1bf1e9e9167721bc98878b
rhysllwydlewis <rhysllwydlewis@gmail.com>
2026-07-19 20:47:21 +0100

    Centralize safe event template metadata cleanup

--- routes/public-listing-seo.js
+++ routes/public-listing-seo.js
@@ -22,13 +22,6 @@ const INDEXABLE_CACHE_CONTROL = 'public, max-age=60, s-maxage=300, stale-while-r
 const NON_INDEXABLE_CACHE_CONTROL = 'public, max-age=30, s-maxage=60, stale-while-revalidate=30';
 const DEFAULT_CACHE_TTL_MS = 60 * 1000;
 
-function removeLegacyEventJsonLd(template) {
-  return String(template || '').replace(
-    /\s*<script\b[^>]*id=["']event-jsonld["'][^>]*>[\s\S]*?<\/script>\s*/gi,
-    '\n'
-  );
-}
-
 function createPublicListingSeoRouter(options = {}) {
   const dbUnified = options.dbUnified;
   const logger = options.logger || require('../utils/logger');
@@ -52,12 +45,10 @@ function createPublicListingSeoRouter(options = {}) {
       const templatePath = kind === 'package' ? PACKAGE_TEMPLATE_PATH : EVENT_TEMPLATE_PATH;
       templatePromises.set(
         kind,
-        fs.readFile(templatePath, 'utf8')
-          .then(template => (kind === 'event' ? removeLegacyEventJsonLd(template) : template))
-          .catch(error => {
-            templatePromises.delete(kind);
-            throw error;
-          })
+        fs.readFile(templatePath, 'utf8').catch(error => {
+          templatePromises.delete(kind);
+          throw error;
+        })
       );
     }
     return templatePromises.get(kind);
@@ -211,4 +202,3 @@ function createPublicListingSeoRouter(options = {}) {
 }
 
 module.exports = createPublicListingSeoRouter;
-module.exports.removeLegacyEventJsonLd = removeLegacyEventJsonLd;
